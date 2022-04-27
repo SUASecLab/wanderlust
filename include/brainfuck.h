622b553d2a2413c2417d100514e1895511e96e6d
stefan <reich.stefan@protonmail.ch>
2022-04-27 15:43:24 +0200

    add GPL3 Licence

--- include/brainfuck.h
+++ include/brainfuck.h
@@ -1,3 +1,22 @@
+/*
+ * Wanderlust is an engine for rpgs rendered in top-down perspective, which
+ * where common in the consoles of the 8- and 16-Bit Era.
+ * Copyright (C) 2022 Stefan Reich
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef BRAINFUCK_CONTEXT
 #define BRAINFUCK_CONTEXT
 
